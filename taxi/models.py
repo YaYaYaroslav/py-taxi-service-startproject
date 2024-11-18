7d6ca097accbb901ad8b2ae9e5cd4a7c3eb1ff05
Ярослав  Полінкін <yaroslav.ua.polinkin@gmail.com>
2024-11-18 08:52:11 +0200

    solution beta

---
+++ taxi/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
+    drivers = models.ManyToManyField("Driver", related_name="cars")
+
+    def __str__(self):
+        return self.model
+
+
+class DriverManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username field must be set")
+        if not email:
+            raise ValueError("The Email field must be set")
+
+        email = self.normalize_email(email)
+        extra_fields.setdefault("is_active", True)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class Driver(AbstractUser):
+    email = models.EmailField(unique=True, null=False, blank=False)
+    license_number = models.CharField(max_length=20, unique=True)
+
+    objects = DriverManager()
+
+    def __str__(self):
+        return f"{self.username} ({self.license_number})"
+
+
