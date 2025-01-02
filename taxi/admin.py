37430c8a99f10251ba1010bbd619f88ce276150b
Ярослав  Полінкін <yaroslav.ua.polinkin@gmail.com>
2025-01-02 10:12:41 +0200

    solution fixed

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,30 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Manufacturer, Car, Driver
 
+from taxi.models import Manufacturer, Driver, Car
 
-@admin.register(Manufacturer)
-class ManufacturerAdmin(admin.ModelAdmin):
-    list_display = ("name", "country")
-    search_fields = ("name",)
+admin.site.register(Manufacturer)
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ("model", "manufacturer")
-    search_fields = ("model",)
-    list_filter = ("manufacturer",)
+    search_fields = ["model", ]
+    list_filter = ["manufacturer", ]
 
 
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
-    fieldsets = (
-        *UserAdmin.fieldsets,
-        ("Additional info", {"fields": ("license_number",)}),
-    )
-    add_fieldsets = (
-        *UserAdmin.add_fieldsets,
-        ("Additional info", {"fields": ("license_number",)}),
-    )
-    list_display = ("username", "email", "first_name", "last_name", "license_number")
-    search_fields = ("username", "email", "license_number")
+    list_display = UserAdmin.list_display + ("license_number", )
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {
+            'fields': ('license_number', )
+        }),)
+    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {
+            'fields': ('license_number', )
+        }),)
--- taxi/models.py
+++ taxi/models.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+from taxi_service.settings import AUTH_USER_MODEL
 
 
 class Manufacturer(models.Model):
@@ -7,51 +9,23 @@ class Manufacturer(models.Model):
     country = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.country}"
 
 
 class Car(models.Model):
-    model = models.CharField(max_length=255)
+    model = models.CharField(max_length=63)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
-    drivers = models.ManyToManyField("Driver", related_name="cars")
+    drivers = models.ManyToManyField(AUTH_USER_MODEL, related_name="cars")
 
     def __str__(self):
-        return self.model
-
-
-class DriverManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not username:
-            raise ValueError("The Username field must be set")
-        if not email:
-            raise ValueError("The Email field must be set")
-
-        email = self.normalize_email(email)
-        extra_fields.setdefault("is_active", True)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self.create_user(username, email, password, **extra_fields)
+        return f"{self.model} {self.manufacturer.name} {self.manufacturer.country}"
 
 
 class Driver(AbstractUser):
-    email = models.EmailField(unique=True, null=False, blank=False)
-    license_number = models.CharField(max_length=20, unique=True)
+    license_number = models.CharField(max_length=63, unique=True)
 
-    objects = DriverManager()
+    class Meta:
+        ordering = ('username',)
 
-    def __str__(self):
-        return f"{self.username} ({self.license_number})"
-
-
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
